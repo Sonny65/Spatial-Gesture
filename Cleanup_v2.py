2e6b3b73d74fe3222e8efd1c72f32a82993fe57f
URDeVotee <Sonny@Yuhans-MacBook-Air.local>
2017-11-23 11:33:16 -0800

    Finished Clean Up
    Ready to calculate rotation, should finish it today

--- Cleanup_v2.py
+++ Cleanup_v2.py
@@ -1,4 +1,7 @@
 import re
+import numpy as np
+from numpy.linalg import inv
+import math
 
 bvh_file_body = "Sentences_1_variations - Body Solve.bvh"
 bvh_file_left = "Sentences_1_variations - Left Hand Solve.bvh"
@@ -25,159 +28,39 @@ scanner = re.Scanner([
     (r"\s+", space),
     ])
 
-def parse_data(bvh,bvh_l,bvh_r,bvh_out, current_token, current_token_left, current_token_right):
+# print out hierachy
 
-	# Parsing Legends Body
+def print_hierarchy(bvh,bvh_l,bvh_r, bvh_out):
 
-	while (bvh[current_token] != ("IDENT", "Time")):
-		bvh_out.write(bvh[current_token][1])
-		if bvh[current_token][1] == "Frames":
-			bvh_out.write(":")
-		current_token = current_token + 1
-
-	bvh_out.write(bvh[current_token][1])
-	bvh_out.write(":")
-	current_token = current_token + 1
-
-	bvh_out.write(bvh[current_token][1])
-	current_token = current_token + 1
-
-	bvh_out.write(bvh[current_token][1])
-	current_token = current_token + 1
-
-	bvh_out.write(bvh[current_token][1])
-	current_token = current_token + 1
-
-	# Parsing Legends Left
-
-	while (bvh_l[current_token_left] != ("IDENT", "Time")):
-		current_token_left = current_token_left + 1
-
-	current_token_left = current_token_left + 1
-
-	current_token_left = current_token_left + 1
-
-	current_token_left = current_token_left + 1
-
-	current_token_left = current_token_left + 1
-
-	# Parsing Legends Right
-
-	while (bvh_r[current_token_right] != ("IDENT", "Time")):
-		current_token_right = current_token_right + 1
-
-	current_token_right = current_token_right + 1
-
-	current_token_right = current_token_right + 1
-
-	current_token_right = current_token_right + 1
-
-	current_token_right = current_token_right + 1
-
-	# Parsing Data
-
-	while (current_token != len(bvh)):
-		count = 0
-		l_count = 0
-		r_count = 0
-		m_count = 0
-
-		while ("\n" not in bvh[current_token][1]):
-			if (bvh[current_token][0] != "DIGIT"):
-				count = count + 1
-			if (count == 51):
-				break
-			bvh_out.write(bvh[current_token][1])
-			current_token = current_token + 1
-
-		bvh_out.write(bvh[current_token][1])
-
-		while ("\n" not in bvh_l[current_token_left][1]):
-			bvh_out.write(bvh_l[current_token_left][1])
-			current_token_left = current_token_left + 1
-
-		while ("\n" not in bvh_l[current_token_left][1]):
-			current_token_left = current_token_left + 1
-
-		current_token_left = current_token_left + 1
-
-		m_count = 0
-
-
-		# Finish Right Collar
-
-		bvh_out.write(bvh[current_token][1])
-
-		while ("\n" not in bvh[current_token][1]):
-			if (bvh[current_token][0] != "DIGIT"):
-				m_count = m_count + 1
-			if (m_count == 4):
-				break
-			current_token = current_token + 1
-
-		current_token = current_token + 1
-
-		m_count = 0
-
-		while ("\n" not in bvh[current_token][1]):
-			if (bvh[current_token][0] != "DIGIT"):
-				m_count = m_count + 1
-			if (m_count == 9):
-				break
-			bvh_out.write(bvh[current_token][1])
-			current_token = current_token + 1
-
-		# Right Hand
-
-		# Get Rid of Extra Degrees
-
-		bvh_out.write(bvh[current_token][1])
-
-		while ("\n" not in bvh_r[current_token_right][1]):
-			bvh_out.write(bvh_r[current_token_right][1])
-			current_token_right = current_token_right + 1
-
-		while ("\n" not in bvh_r[current_token_right][1]):
-			current_token_right = current_token_right + 1
-
-		current_token_right = current_token_right + 1
-
-		# Transfer to New Frame
-
-		while ("\n" not in bvh[current_token][1]):
-			# bvh_out.write(bvh[current_token][1])
-			current_token = current_token + 1
-
-		bvh_out.write(bvh[current_token][1])
-		current_token = current_token + 1
-
-
-def parse_hierarchy(bvh,bvh_l,bvh_r):
-	bvh_out = open(bvh_out_file, "w")
-
-	global current_token
-	global current_token_left
+	# tokens to keep track of writting flow
 
 	current_token = 0
 	current_token_left = 0
 	current_token_right = 0
 
+	# print out orginal hierarchy
 
-	while (bvh[current_token-1] != ("IDENT", "L_Wrist")):
+	while (bvh[current_token] != ("IDENT", "L_Wrist")):
+		bvh_out.write(bvh[current_token][1])
+		current_token = current_token + 1
+
+	# using the lewrist from body
+
+	while (bvh[current_token] != ("IDENT", "End")):
 		bvh_out.write(bvh[current_token][1])
 		current_token = current_token + 1
 
 	# Left Hand
 
-	while (bvh[current_token] != ("IDENT", "End")):
-		# bvh_out.write(bvh[current_token][1])
-		current_token = current_token + 1
-
 	left_space = bvh[current_token-1][1]
+	bvh_out.write("JOINT ")
 
-	while (bvh_l[current_token_left] != ("IDENT", "leWrist")):
+	while (bvh_l[current_token_left] != ("IDENT", "leHand")):
 		current_token_left = current_token_left + 1
 
+	# calculate the space
+
+	bvh_out.write(bvh_l[current_token_left][1])
 	current_token_left = current_token_left + 1
 	left_space = left_space.replace(bvh_l[current_token_left][1], '')
 
@@ -206,17 +89,19 @@ def parse_hierarchy(bvh,bvh_l,bvh_r):
 		bvh_out.write(bvh[current_token][1])
 		current_token = current_token + 1
 
-	bvh_out.write(bvh[current_token][1])
-
 	while (bvh[current_token] != ("IDENT", "End")):
-		# bvh_out.write(bvh[current_token][1])
+		bvh_out.write(bvh[current_token][1])
 		current_token = current_token + 1
 
-	right_space = bvh[current_token-1][1]
+	# calculate the space
 
-	while (bvh_r[current_token_right] != ("IDENT", "wrist")):
+	right_space = bvh[current_token-1][1]
+	bvh_out.write("JOINT ")
+
+	while (bvh_r[current_token_right] != ("IDENT", "riHand")):
 		current_token_right = current_token_right + 1
 
+	bvh_out.write(bvh_r[current_token_right][1])
 	current_token_right = current_token_right + 1
 	right_space = right_space.replace(bvh_r[current_token_right][1], '')
 
@@ -228,8 +113,6 @@ def parse_hierarchy(bvh,bvh_l,bvh_r):
 			break
 		current_token_right = current_token_right + 1
 
-	current_token_right = current_token_right + 1
-
 	while (bvh[current_token][0] != "CLOSE_BRACE"):
 		current_token = current_token + 1
 	current_token = current_token + 1
@@ -238,21 +121,188 @@ def parse_hierarchy(bvh,bvh_l,bvh_r):
 		current_token = current_token + 1
 	current_token = current_token + 1
 
-	#END OF RIGHT HAND
-	while (bvh_l[current_token_left] != ("IDENT", "Time")):
-		current_token_left = current_token_left + 1
+	# finish the body
 
-	while (bvh_r[current_token_right] != ("IDENT", "Time")):
-		current_token_right = current_token_right + 1
-
-	while (bvh[current_token] != ("IDENT", "MOTION")):
+	while (bvh[current_token] != ("IDENT", "Time")):
 		bvh_out.write(bvh[current_token][1])
 		current_token = current_token + 1
 
-	# PARSING DATA
-	parse_data(bvh,bvh_l,bvh_r,bvh_out, current_token, current_token_left, current_token_right)
+	while ("\n" not in bvh[current_token][1]):
+		bvh_out.write(bvh[current_token][1])
+		current_token = current_token + 1
 
-	bvh_out.close()
+	bvh_out.write(bvh[current_token][1])
+
+def analyse_hierarchy(bvh,bvh_l,bvh_r):
+
+	# tokens to keep track of parsing flow
+
+	current_token = 0
+	current_token_left = 0
+	current_token_right = 0
+
+	data_index = 0
+	data_index_mid = 0
+	data_index_left = 0
+	data_index_right = 0
+
+	# Calculate the index in body
+
+	while (bvh[current_token] != ("IDENT", "L_Wrist")):
+		if "rotation" in bvh[current_token][1]:
+			data_index = data_index + 1
+		current_token = current_token + 1
+
+	while (bvh[current_token] != ("IDENT", "End")):
+		if "rotation" in bvh[current_token][1]:
+			data_index = data_index + 1
+		current_token = current_token + 1
+
+	while (bvh[current_token] != ("IDENT", "R_Wrist")):
+		if "rotation" in bvh[current_token][1]:
+			data_index_mid = data_index_mid + 1
+		current_token = current_token + 1
+
+	while (bvh[current_token] != ("IDENT", "End")):
+		if "rotation" in bvh[current_token][1]:
+			data_index_mid = data_index_mid + 1
+		current_token = current_token + 1
+
+	# Calculate the left wrists
+
+	while (bvh_l[current_token_left] != ("IDENT", "leHand")):
+		current_token_left = current_token_left + 1
+
+	while (bvh_l[current_token_left] != ("IDENT", "MOTION")):
+		if "rotation" in bvh_l[current_token_left][1]:
+			data_index_left = data_index_left + 1
+		current_token_left = current_token_left + 1
+
+	# Calculate the right wrists
+
+	while (bvh_r[current_token_right] != ("IDENT", "riHand")):
+		current_token_right = current_token_right + 1
+
+	while (bvh_r[current_token_right] != ("IDENT", "MOTION")):
+		if "rotation" in bvh_r[current_token_right][1]:
+			data_index_right = data_index_right + 1
+		current_token_right = current_token_right + 1
+
+	return data_index, data_index_mid, data_index_left, data_index_right
+
+def organize_frames(bvh,bvh_l,bvh_r):
+
+	# store the frames
+
+	body_frames = []
+	left_frames = []
+	right_frames = []
+
+	body_frame = []
+	left_frame = []
+	right_frame = []
+
+	# tokens to keep track of parsing flow
+
+	current_token = 0
+	current_token_left = 0
+	current_token_right = 0
+
+	# getting to data
+
+	while (bvh[current_token] != ("IDENT", "Frame")):
+		current_token = current_token + 1
+
+	while ("\n" not in bvh[current_token][1]):
+		current_token = current_token + 1
+
+	while (bvh_l[current_token_left] != ("IDENT", "Frame")):
+		current_token_left = current_token_left + 1
+
+	while ("\n" not in bvh_l[current_token_left][1]):
+		current_token_left = current_token_left + 1
+
+	while (bvh_r[current_token_right] != ("IDENT", "Frame")):
+		current_token_right = current_token_right + 1
+
+	while ("\n" not in bvh_r[current_token_right][1]):
+		current_token_right = current_token_right + 1
+
+	# go to the first line of frames
+
+	current_token = current_token + 1
+	current_token_left = current_token_left + 1
+	current_token_right = current_token_right + 1
+
+	# creating the new frames
+
+	while (current_token+1<len(bvh)):
+		body_frame = []
+		while ("\n" not in bvh[current_token][1]):
+			if (bvh[current_token][0] == "DIGIT"):
+				body_frame = body_frame + [float(bvh[current_token][1])]
+			current_token = current_token + 1
+		body_frames = body_frames + [body_frame]
+		current_token = current_token + 1
+
+	while (current_token_left+1<len(bvh_l)):
+		left_frame = []
+		while ("\n" not in bvh_l[current_token_left][1]):
+			if (bvh_l[current_token_left][0] == "DIGIT"):
+				left_frame = left_frame + [float(bvh_l[current_token_left][1])]
+			current_token_left = current_token_left + 1
+		left_frames = left_frames + [left_frame]
+		current_token_left = current_token_left + 1
+
+	while (current_token_right+1<len(bvh_r)):
+		right_frame = []
+		while ("\n" not in bvh_r[current_token_right][1]):
+			if (bvh_r[current_token_right][0] == "DIGIT"):
+				right_frame = right_frame + [float(bvh_r[current_token_right][1])]
+			current_token_right = current_token_right + 1
+		right_frames = right_frames + [right_frame]
+		current_token_right = current_token_right + 1
+
+	return body_frames, right_frames, left_frames
+
+def wrist_rotation(bvh, body_frames, right_frames, left_frames):
+
+	# copy frames
+
+	bf = []
+	rf = []
+	lf = []
+
+	# get rid of positions from frames
+
+	for x in body_frames:
+		bf = bf + [x[3:]]
+
+	for x in right_frames:
+		rf = rf + [x[3:]]
+
+	for x in left_frames:
+		lf = lf + [x[3:]]
+
+	# tokens to keep track of parsing flow
+	
+	current_token = 0
+
+	# create parent
+
+	body = []
+	parent_left = []
+	parent_right = []
+
+	# creating parent from body file
+
+	# root is a special case
+
+	while (bvh[current_token] != ("IDENT", "L_Femur")):
+		if "rotation" in bvh[current_token][1]:
+			body = body + [bvh[current_token][1]]
+		current_token = current_token + 1
+
 
 if __name__ == "__main__":
 	bvh_file_body = open(bvh_file_body, "r")
@@ -271,4 +321,26 @@ if __name__ == "__main__":
 	tokens_left, remainder = scanner.scan(bvh_left)
 	tokens_right, remainder = scanner.scan(bvh_right)
 
-	parse_hierarchy(tokens_body,tokens_left, tokens_right)
+	bvh_out = open(bvh_out_file, "w")
+
+	# print out hierachy
+
+	print_hierarchy(tokens_body,tokens_left, tokens_right, bvh_out)
+
+	# get the index
+
+	data_index, data_index_mid, data_index_left, data_index_right = analyse_hierarchy(tokens_body,tokens_left, tokens_right)
+
+	# extract frames
+
+	body_frames, right_frames, left_frames = organize_frames(tokens_body,tokens_left, tokens_right)
+
+	# calculate the wrist rotation
+
+	print len(body_frames[0])
+
+	wrist_rotation(tokens_body, body_frames, right_frames, left_frames)
+
+	print len(body_frames[0])
+
+	bvh_out.close()
