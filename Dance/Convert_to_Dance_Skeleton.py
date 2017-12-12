71612bcf8101f8af140171ab4cf766a7df672eb9
URDeVotee <Sonny@Yuhans-MacBook-Air.local>
2017-12-12 21:17:42 +0900

    reorder

--- Dance/Convert_to_Dance_Skeleton.py
+++ Dance/Convert_to_Dance_Skeleton.py
@@ -86,7 +86,24 @@ def organize_finger(bvh,bvh_out,token):
 
     # write out the thumb
 
+    prox = False
+
     while (bvh[current_token] != ("IDENT", "Site")):
+
+        # change the order of the rotations
+
+        if ("Prox" in bvh[current_token][1]):
+            prox = True
+
+        if (bvh[current_token+4] == ("IDENT", "Xrotation") and prox):
+            bvh_out.write("CHANNELS 3 Yrotation Zrotation Xrotation")
+            current_token = current_token + 9
+            prox = False
+        elif (bvh[current_token+4] == ("IDENT", "Xrotation")):
+            bvh_out.write("CHANNELS 1 Zrotation")
+            current_token = current_token + 9
+            prox = False
+
         bvh_out.write(bvh[current_token][1])
         current_token = current_token + 1
 
@@ -108,7 +125,24 @@ def organize_finger(bvh,bvh_out,token):
 
     current_token = token
 
+    prox = False
+
     while ("1ProxPhalanx" not in bvh[current_token+3][1]):
+
+        # change the order of the rotations
+
+        if ("Prox" in bvh[current_token][1]):
+            prox = True
+
+        if (bvh[current_token+4] == ("IDENT", "Zrotation") and prox):
+            bvh_out.write("CHANNELS 2 Yrotation Zrotation")
+            current_token = current_token + 9
+            prox = False
+        elif (bvh[current_token+4] == ("IDENT", "Xrotation")):
+            bvh_out.write("CHANNELS 1 Zrotation")
+            current_token = current_token + 9
+            prox = False
+
         bvh_out.write(bvh[current_token][1])
         current_token = current_token + 1
 
@@ -126,6 +160,8 @@ def organize_finger(bvh,bvh_out,token):
 # write out thighs
 def write_thighs(bvh, bvh_out):
 
+    # control the parsing flow
+
     current_token = 0
 
     # go to thighs
@@ -142,8 +178,233 @@ def write_thighs(bvh, bvh_out):
         current_token = current_token + 1
 
 # write out motion data
+def write_motion(bvh, bvh_out):
 
+    # control the parsing flow
 
+    current_token = 0
+
+    # write legend
+
+    while (bvh[current_token] != ("IDENT", "MOTION")):
+        current_token = current_token + 1
+
+    while (bvh[current_token] != ("IDENT", "Time")):
+        bvh_out.write(bvh[current_token][1])
+        current_token = current_token + 1
+
+    for x in range(0, 4):
+        bvh_out.write(bvh[current_token][1])
+        current_token = current_token + 1
+        x
+
+    write_frames(bvh, bvh_out, current_token)
+
+def write_frames(bvh, hvh_out, token):
+
+    # control the parsing flow
+
+    current_token = token + 1
+
+    frames = []
+
+    # read frames
+
+    while (current_token < len(bvh)):
+        frame = []
+        while ("\n" not in bvh[current_token][1]):
+            if (bvh[current_token][0] == "DIGIT"):
+                frame = frame + [bvh[current_token][1]]
+            current_token = current_token + 1
+        current_token = current_token + 1
+        frames = frames + [frame]
+
+    bvh_out.write("\n")
+
+    for frame in frames:
+        # print len(frame)
+        write_single_frame(frame)
+        bvh_out.write("\n")
+
+def write_single_frame(frame):
+
+    # write out hip
+
+    for x in range(0, 6):
+        bvh_out.write(frame[x])
+        bvh_out.write(" ")
+        x
+
+    # write out abdomen
+
+    for x in range(20, 41):
+        bvh_out.write(frame[x])
+        bvh_out.write(" ")
+        x
+
+    # write out left thumb:
+
+    bvh_out.write(frame[78])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[79])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[77])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[82])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[85])
+    bvh_out.write(" ")
+
+    # write out rest of the fingers
+
+    # L2
+
+    bvh_out.write(frame[42])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[43])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[46])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[49])
+    bvh_out.write(" ")
+
+    # L3
+
+    bvh_out.write(frame[51])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[52])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[55])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[58])
+    bvh_out.write(" ")
+
+    # L4
+
+    bvh_out.write(frame[60])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[61])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[64])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[67])
+    bvh_out.write(" ")
+
+    # L5
+
+    bvh_out.write(frame[69])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[70])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[73])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[76])
+    bvh_out.write(" ")
+
+    # write out right hand
+
+    for x in range(86, 95):
+        bvh_out.write(frame[x])
+        bvh_out.write(" ")
+        x
+
+    # write out right thumb:
+
+    bvh_out.write(frame[132])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[133])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[131])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[136])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[139])
+    bvh_out.write(" ")
+
+    # write out rest of the fingers
+
+    # R2
+
+    bvh_out.write(frame[96])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[97])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[100])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[103])
+    bvh_out.write(" ")
+
+    # R3
+
+    bvh_out.write(frame[105])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[106])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[109])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[112])
+    bvh_out.write(" ")
+
+    # R4
+
+    bvh_out.write(frame[114])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[115])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[118])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[121])
+    bvh_out.write(" ")
+
+    # R5
+
+    bvh_out.write(frame[123])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[124])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[127])
+    bvh_out.write(" ")
+
+    bvh_out.write(frame[130])
+    bvh_out.write(" ")
+
+    # write out thighs
+
+    for x in range(6, 20):
+        bvh_out.write(frame[x])
+        bvh_out.write(" ")
+        x
 
 if __name__ == "__main__":
     bvh_file = open(bvh_file, "r")
@@ -160,6 +421,10 @@ if __name__ == "__main__":
 
     # write out last brace
 
-    bvh_out.write("\n}")
+    bvh_out.write("\n}\n")
+
+    # write frames
+
+    write_motion(tokens, bvh_out)
 
     bvh_out.close()
